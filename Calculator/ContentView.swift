fd6dd5060840f40924c965e7fcc81186c53be326
yuqiang <1054572107@qq.com>
2019-12-14 13:42:15 +0800

    calculator UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,85 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let titleColorName: String
+    let action: () -> Void
+
     var body: some View {
-        Text("Hello World")
+        Button(action: action) {
+            Text(title)
+            .font(.system(size: fontSize))
+            .foregroundColor(Color(titleColorName))
+            .frame(width: size.width, height: size.height, alignment: .center)
+            .background(Color(backgroundColorName))
+            .cornerRadius(size.width / 2)
+        }
+    }
+}
+
+struct CalculatorButtonRow: View {
+    let row: [CalculatorButtonItem]
+
+    var body: some View {
+        HStack {
+            ForEach(row, id: \.self) { (item) in
+                CalculatorButton(title: item.title, size: item.size, backgroundColorName: item.backgroundColorName, titleColorName: item.titleColorName) {
+                    print("Button: \(item.title)")
+                }
+            }
+        }
+    }
+}
+
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide),],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)]
+    ]
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 8) {
+            ForEach(pad, id: \.self) { (row) -> CalculatorButtonRow in
+                CalculatorButtonRow(row: row)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    let scale: CGFloat = UIScreen.main.bounds.width / 414
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Spacer()
+            Text("0")
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24)
+                .lineLimit(1)
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    alignment: .trailing)
+            CalculatorButtonPad()
+                .padding(.bottom)
+        }
+        .scaleEffect(scale)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView().previewDevice("iPhone SE")
+        }
     }
 }
